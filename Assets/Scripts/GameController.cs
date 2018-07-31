9be915ed0e0b72bffebab18c3513e5ccd58d4926
Thiago Zacarias <tzacarias@NCGames.local>
2018-07-31 18:43:21 -0300

    Movimentação inicial

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+	public float score, time;
+	public Text txtScore, txtTime;
+	private float _tempTime, _seconds;
 
 	void Start () {
-		
+		_tempTime = time;
+		_seconds = 60;
+		if(_tempTime % 60 != 0){
+			_seconds = _tempTime % 60;
+		}
 	}
 	
 	
 	void Update () {
-		
+		UpdateTime();
+	}
+
+	private void UpdateTime(){
+		_tempTime -= Time.deltaTime;
+		_seconds -= Time.deltaTime;
+		txtTime.text = (Mathf.FloorToInt(_tempTime)/60).ToString();
+		txtTime.text = txtTime.text + ":" +  Mathf.FloorToInt(_seconds);
+		if(_seconds <= 0)
+			_seconds = 60;
+	}
+
+	private void ConvertTime(){
+		Mathf.FloorToInt(_tempTime);
+		//int _minute = Mathf.FloorToInt(seconds);
+
 	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float playerSpeed;
+	private string _CurrentPlayerPosition = "middle";
+	public GameObject[] positions;
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		_CurrentPlayerPosition = DetectChangePosition(_CurrentPlayerPosition);
+		PlayerMoviment();
+	}
+
+	private void PlayerMoviment(){
+		string[] _tempDirections = new string[]{"down", "middle", "up"};
+		for(int i = 0; i < positions.Length; i++){
+			if (_tempDirections[i] == _CurrentPlayerPosition){
+				transform.localPosition = Vector3.MoveTowards(transform.position, positions[i].transform.position, playerSpeed * Time.deltaTime);
+			}
+		}
+		
+	}
+
+	private string DetectChangePosition(string _playerPosition){
+		float _Direction;
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			_Direction = 1;
+		} else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			_Direction = -1;
+		}else{
+			_Direction = 0;
+		}
+
+		switch(_playerPosition){
+			case "middle":
+				if (_Direction > 0){
+					return "up";
+				}else if (_Direction < 0){
+					return "down";
+				}else{
+					return "middle";
+				}
+			case "down":
+				if(_Direction > 0){
+					return "middle";
+				}else{
+					return "down";
+				}
+			case "up":
+				if(_Direction < 0){
+					return "middle";
+				}else{
+					return "up";
+				}	
+		}
+		return _playerPosition;
+	}
+}
